d54bbd849258fc59cc7ba3773a9e3399a2fd76a0
Hemanth Sreenadhuni <hemanthsreenadhuni@gmail.com>
2026-03-15 17:03:51 +0530

    update: anonymous, factory, closure and variadic functions

--- 07-More-on-Functions/app.go
+++ 07-More-on-Functions/app.go
@@ -3,15 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4}
+	numbers := []int{1, 2, 3, 4, 5}
 	doubled := transformNumbers(&numbers, getTransformFn(2)) // --> passing function as an argument
 	tripled := transformNumbers(&numbers, getTransformFn(3))
 
 	quadrupled := transformNumbers(&numbers, createTransformer(4))
+	fiveFactorial := factorial(5)
 
-	fmt.Println(doubled)
-	fmt.Println(tripled)
-	fmt.Println(quadrupled)
+	sum3 := sumUp(1, 2, 3)
+	sum4 := sumUp(1, 2, 3, 4)
+	sum5 := sumUp(numbers[0], numbers[1:]...)
+
+	fmt.Println("numbers: ", numbers)
+	fmt.Println("doubled: ", doubled)
+	fmt.Println("tripled: ", tripled)
+	fmt.Println("quadrupled: ", quadrupled)
+	fmt.Println("fiveFactorial: ", fiveFactorial)
+	fmt.Println("sum3: ", sum3)
+	fmt.Println("sum4: ", sum4)
+	fmt.Println("sum5: ", sum5)
 }
 
 type TTransformFn func(int) int
@@ -23,17 +33,17 @@ func transformNumbers(numbers *[]int, transformFn TTransformFn) []int { // --> t
 		dNum = append(dNum, transformFn(val))
 	}
 
-	return dNum;
+	return dNum
 }
 
 func getTransformFn(num int) TTransformFn {
 	if num == 2 {
-		return func (num int) int {  // --> anonymous function
+		return func(num int) int { // --> anonymous function
 			return num * 2
 		}
 		// return doubleInt
 	}
-	return func (num int) int {  // --> anonymous function
+	return func(num int) int { // --> anonymous function
 		return num * 3
 	}
 	// return tripleInt
@@ -49,6 +59,23 @@ func getTransformFn(num int) TTransformFn {
 
 func createTransformer(factor int) TTransformFn { // --> factory function
 	return func(number int) int {
-		return number * factor; // --> closure, being able to use factor inside this anonymous function that's returned, factor –– the parameter of createTransformer func, is in the scope of this anon func
- 	}
+		return number * factor // --> closure, being able to use factor inside this anonymous function that's returned, factor –– the parameter of createTransformer func, is in the scope of this anon func
+	}
+}
+
+func factorial(number int) int {
+	if number == 2 {
+		return number
+	}
+	return number * factorial(number-1)
+}
+
+func sumUp(start int, numbers ...int) int { // --> variadic function
+	sum := start
+
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return sum
 }
